cd9b3d1519cf2dbb46138fc52ee8ee4f6b25d505
ramilatagiyeva <tagiyevaramila02@gmail.com>
2024-12-25 16:35:14 +0400

    Add files via upload

---
+++ FirstSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FirstSection.css'; 
+const FirstSection = () => {
+  return (
+    <section className="First">
+      <div className="container">
+        <div className="first-content">
+          <h4>SEO DIGITAL AGENCY</h4>
+          <h1>Dive Into The SEO World With Tale</h1>
+          <p>Tale is the best SEO agency website template using Bootstrap v5.2.2 CSS for your company. It is a free download provided by TemplateMo. There are 3 HTML pages, Home, About, and FAQ.</p>
+          <button className='one'>Discover More</button>
+          <button className='two'>Check Our FAQs</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default FirstSection;
